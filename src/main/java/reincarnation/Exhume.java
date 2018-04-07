08e24c4c303d38fab76357dd7fa6b714676d2a55
Teletha <Teletha.T@gmail.com>
2018-04-08 08:41:52 +0900

    Progress...

---
+++ src/main/java/reincarnation/Exhume.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2018 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package reincarnation;
+
+/**
+ * @version 2018/04/07 8:36:45
+ */
+public class Exhume {
+
+}
