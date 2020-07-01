69d90a8e7913b95611f72a5dbc068fad82b58c8e
Teletha <Teletha.T@gmail.com>
2020-07-01 13:10:53 +0900

    Fix license description.

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Reincarnation Development Team
+ * Copyright (C) 2020 Reincarnation Development Team
  *
  * Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,8 @@ public class Project extends bee.api.Project {
         product("com.github.teletha", "Reincarnation", "0.6");
 
         require("org.ow2.asm", "asm");
-        require("com.github.teletha", "sinobu");
+        require("com.github.teletha", "sinobu", "[2.0,)");
         require("com.github.teletha", "antibug").atTest();
-        require("com.github.teletha", "Marionette").atTest();
         require("com.github.teletha", "psychopath").atTest();
     }
 }
