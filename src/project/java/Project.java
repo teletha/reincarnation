66a5feda9b000211def691380f66ff2741dde57e
Teletha <Teletha.T@gmail.com>
2021-08-25 16:39:55 +0900

    Update pom.

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -13,7 +13,7 @@ public class Project extends bee.api.Project {
         product("com.github.teletha", "Reincarnation", "0.6");
 
         require("org.ow2.asm", "asm");
-        require("com.github.teletha", "sinobu", "[2.0,)");
+        require("com.github.teletha", "sinobu");
         require("com.github.teletha", "antibug").atTest();
         require("com.github.teletha", "psychopath").atTest();
     }
