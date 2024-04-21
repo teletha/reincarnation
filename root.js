849785a20aa4c3243384698cd6593f700eeed13a
teletha <teletha@users.noreply.github.com>
2024-04-21 03:13:01 +0000

    deploy: 017f484aff0f6fef8c268088ff33bb9360925e17

---
+++ root.js
@@ -0,0 +1,259 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"reincarnation",
+		"reincarnation.coder",
+		"reincarnation.coder.java",
+		"reincarnation.operator",
+		"reincarnation.structure",
+		"reincarnation.util"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AccessMode",
+			"packageName": "reincarnation.operator",
+			"type": "Enum"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AssignOperator",
+			"packageName": "reincarnation.operator",
+			"type": "Enum"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "BinaryOperator",
+			"packageName": "reincarnation.operator",
+			"type": "Enum"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Break",
+			"packageName": "reincarnation.structure",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Breakable",
+			"packageName": "reincarnation.structure",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Classes",
+			"packageName": "reincarnation.util",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Code",
+			"packageName": "reincarnation.coder",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Coder",
+			"packageName": "reincarnation.coder",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "CodingOption",
+			"packageName": "reincarnation.coder",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Continue",
+			"packageName": "reincarnation.structure",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Debuggable",
+			"packageName": "reincarnation",
+			"type": "Annotation"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "DelegatableCoder",
+			"packageName": "reincarnation.coder",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "DoWhile",
+			"packageName": "reincarnation.structure",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "For",
+			"packageName": "reincarnation.structure",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Fragment",
+			"packageName": "reincarnation.structure",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "GeneratedCodes",
+			"packageName": "reincarnation.util",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "If",
+			"packageName": "reincarnation.structure",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "InfiniteLoop",
+			"packageName": "reincarnation.structure",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "JavaCoder",
+			"packageName": "reincarnation.coder.java",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "JavaCodingOption",
+			"packageName": "reincarnation.coder.java",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Join",
+			"packageName": "reincarnation.coder",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Jumpable",
+			"packageName": "reincarnation.structure",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Loopable",
+			"packageName": "reincarnation.structure",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "MultiMap",
+			"packageName": "reincarnation.util",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Naming",
+			"packageName": "reincarnation.coder",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Node",
+			"packageName": "reincarnation",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "NodeManipulator",
+			"packageName": "reincarnation",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Operand",
+			"packageName": "reincarnation",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Reincarnation",
+			"packageName": "reincarnation",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Structure",
+			"packageName": "reincarnation.structure",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Switch",
+			"packageName": "reincarnation.structure",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Try",
+			"packageName": "reincarnation.structure",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "UnaryOperator",
+			"packageName": "reincarnation.operator",
+			"type": "Enum"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "VariableNaming",
+			"packageName": "reincarnation.coder",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "While",
+			"packageName": "reincarnation.structure",
+			"type": "Class"
+		}
+	]
+}
