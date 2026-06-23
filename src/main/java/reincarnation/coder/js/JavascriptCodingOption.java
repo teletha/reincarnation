89e9df921ececbb0778872e5f3db7f60571f5acd
Teletha <Teletha.T@gmail.com>
2026-06-24 07:26:58 +0900

    wip

---
+++ src/main/java/reincarnation/coder/js/JavascriptCodingOption.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2024 The REINCARNATION Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
+package reincarnation.coder.js;
+
+import reincarnation.coder.CodingOption;
+
+public class JavascriptCodingOption extends CodingOption {
+
+    /** The write member class from top level class, this is mainly used for test. */
+    public boolean writeMemberFromTopLevel;
+}
